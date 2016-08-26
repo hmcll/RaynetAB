234550540bbcfbdbc73af2b23d68c624da2436d0
Michael Huo <hmcll5555@sina.com>
2016-08-26 23:17:20 +0800

    Modified Assets

--- RaynetAB_Test13/Build/Android/src/com/YourCompany/RaynetAB_Test13/OBBData.java
+++ RaynetAB_Test13/Build/Android/src/com/YourCompany/RaynetAB_Test13/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-138084435L // the length of the file in bytes
+129490879L // the length of the file in bytes
 )
 };
 };
