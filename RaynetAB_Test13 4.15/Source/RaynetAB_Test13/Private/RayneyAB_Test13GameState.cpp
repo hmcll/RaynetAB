974e9c83157e2761bbe2210bd609a0f9335722a1
Michael Huo <hmcll5555@sina.com>
2017-05-14 18:43:12 +0800

    Add Empty GameState

---
+++ RaynetAB_Test13 4.15/Source/RaynetAB_Test13/Private/RayneyAB_Test13GameState.cpp
@@ -0,0 +1,8 @@
+// Copyright(C) 2015 - 2016  Raynet AB Development Team
+
+#include "RaynetAB_Test13.h"
+#include "RayneyAB_Test13GameState.h"
+
+
+
+
---
+++ RaynetAB_Test13 4.15/Source/RaynetAB_Test13/Public/RayneyAB_Test13GameState.h
@@ -0,0 +1,19 @@
+// Copyright(C) 2015 - 2016  Raynet AB Development Team
+
+#pragma once
+
+#include "GameFramework/GameStateBase.h"
+#include "RayneyAB_Test13GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAYNETAB_TEST13_API ARayneyAB_Test13GameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
