ab019588d825711ce26a897ce76633a0c6e99964
Michael Huo <hmcll5555@sina.com>
2017-06-28 21:02:16 +0800

    Revert "Add GameState"
    This reverts commit d8015223857cf29f0d2c25a8900b9fbd01717d6b.

--- RaynetAB_Test13 4.15/Source/RaynetAB_Test13/Public/RayneyAB_Test13GameState.h
+++ RaynetAB_Test13 4.15/Source/RaynetAB_Test13/Public/RayneyAB_Test13GameState.h
@@ -4,7 +4,6 @@
 
 #include "GameFramework/GameStateBase.h"
 #include "GamePlayer.h"
-#include "Net/UnrealNetwork.h"
 #include "RayneyAB_Test13GameState.generated.h"
 
 /**
@@ -27,30 +26,12 @@ struct FMove_C {
 		TerminalCard Type;
 };
 
-UENUM(BlueprintType)
-enum class EGameState : uint8 {
-	Waiting,
-	Starting,
-	Setting_BothNotFinished,
-	Setting_OneSideNotFinished,
-	Setting_BothFinished,
-	Playing,
-	Ending
-};
 
 UCLASS()
 class RAYNETAB_TEST13_API ARayneyAB_Test13GameState : public AGameStateBase
 {
 	GENERATED_BODY ()
 public:
-	UPROPERTY (Replicated)
-		bool Flag_OnesideFinished = false;
-
-	UPROPERTY ( BlueprintReadWrite, Replicated, Category = "Variable" )
-		EGameState state = EGameState::Waiting;
-
-	ARayneyAB_Test13GameState ();
-
 	UFUNCTION ( Server, Reliable, WithValidation , Category = "Function" )
 		void Server_SetPawn_Arrangement (bool Offensive,const TArray<ShowType>& Setting);
 	
